7ac549185311ab7a10fc4d54833be1d1cc7995fc
Ken Zhang <yzhang76@uvm.edu>
2020-04-05 17:16:20 -0400

    almost

--- unittests.py
+++ unittests.py
@@ -9,7 +9,6 @@ class TestTreatment(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # called one time, at beginning
         print('setUpClass()')
         cls.hospital = hospital.Hospital().get()
         p_name_1 = 'Jack'
@@ -29,7 +28,7 @@ class TestTreatment(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        # called one time, at end
+        # called at end
         print('tearDownClass()')
 
     def setUp(self):
@@ -40,55 +39,45 @@ class TestTreatment(unittest.TestCase):
         # called after every test
         print('tearDown()')
 
-    # -------------------------------------------------------------
-
     def test_treatment_one(self):
-        # check that the hospital shows that no patients are checked out to john
+        # check that the hospital shows that no patients are in treatment from Dr. Strange
         patients = self.hospital.get_treatments(self.strange)
         self.assertEqual(len(patients), 0)
 
-        # check out a patient to john
+        # Dr. Strange has a patient
         t = self.hospital.do_treatment(self.strange, self.patient1)
 
-        # check that the hospital shows one patient checked out to john
+        # check that the hospital shows one patient is in treatment from Dr. Strange
         patients = self.hospital.get_treatments(self.strange)
         self.assertEqual(len(patients), 1)
 
-        # check that the patient checked out to john is patient1
+        # check that the patient with Dr. Strange is patient1
         if len(patients) == 1:
             self.assertEqual(patients[0], self.patient1)
 
-        # check that john shows one patient checked out
+        # check that Dr. Strange has one patient in treatment
         patients = self.strange.get_treatments()
         self.assertEqual(len(patients), 1)
 
-        # check that the patient checked out to john is patient1
+        # check that the patient with Dr. Strange is patient1
         if len(patients) == 1:
             self.assertEqual(patients[0], self.patient1)
-
-    # -------------------------------------------------------------
 
     def test_recover(self):
-        # return john's patient--should return True
+        # return Dr. Strange's patient--should return False
         r = self.hospital.recover(self.strange, self.patient1)
-        self.assertTrue(r)
+        self.assertFalse(r)
 
-        # try to return the same patient again--should return False
-        rc = self.hospital.recover(self.strange, self.patient1)
-        self.assertFalse(rc)
+        # return the same patient again--should return False
+        r = self.hospital.recover(self.strange, self.patient1)
+        self.assertFalse(r)
 
-        # check that the hospital shows that john has no patients checked out
+        # check that the hospital shows that Dr. Strange has no patients
         patients = self.hospital.get_treatments(self.strange)
         self.assertEqual(len(patients), 0)
 
-        # check that john shows no patients checked out
-        # right now, this code is failing, because my code is incorrect--
-        # I'm returning to the hospital but not to Patron
-        patients = self.strange.get_treatments()
-        self.assertEqual(len(patients), 0)
 
 
-# -----------------------------------------
 
 if __name__ == "__main__":
     unittest.main()
